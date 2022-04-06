43546cfcc669e5c3466b68f451aeff0f10352e14
Rafał Kochanecki <rafal.kochanecki@gmail.com>
2022-04-06 18:17:13 +0200

    task 1 completed

---
+++ 01/app.js
@@ -0,0 +1,11 @@
+user = {
+    firstName: 'Rafal',
+    lastName: "Kochanecki",
+    sex: 'male',
+    age: 34
+}
+
+console.log(user.firstName)
+console.log(user.lastName)
+console.log(user.sex)
+console.log(user.age)
